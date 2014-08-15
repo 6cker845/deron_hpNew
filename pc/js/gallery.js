1bba22ea0809187647e85c58abf367bcf7a3c31d
6cker845 <nick_fun6king@yahoo.co.jp>
2014-08-15 13:07:05 +0900

    jsonロードのコードをgallery.jsに移動

--- pc/js/gallery.js
+++ pc/js/gallery.js
@@ -1,6 +1,9 @@
-
-$.getJSON('../js/json/images.json', null, function(json) {
-	for (var i=0; i<json.length; i++) {
-		console.log(json[i].image);
-	}
+	$.getJSON('../js/json/images.json', null, function(json) {
+		var ul = $('<ul>')
+		for (var i=0; i<json.length; i++) {
+			ul.append('<li><img src="' + json[i].image + '" width="100"></a><br><p>' + json[i].label + '</p></li>');
+			console.log(json[i].image);
+		}
+		
+		$('#slideshow').html(ul);
 });
